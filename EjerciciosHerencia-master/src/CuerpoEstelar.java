be10cdcdd2243e809465276c49a328da33591d85
IzliaB <gfbane23@gmail.com>
2016-11-02 09:59:58 -0600

    Tarea Herencia

---
+++ EjerciciosHerencia-master/src/CuerpoEstelar.java
@@ -0,0 +1,13 @@
+
+public class CuerpoEstelar {
+int posicion;
+int masa;
+
+CuerpoEstelar(int posicion, int masa){
+	this.posicion = posicion;
+	this.masa = masa;
+ }
+ void funcionPadre(){
+	System.out.println("Funcion del padre");
+}
+}
